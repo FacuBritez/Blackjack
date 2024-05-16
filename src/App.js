b02f3acd222248e1ce30ac9da45d19d2fdb4c9a0
FacuBritez <FacundoIvanBritez@gmail.com>
2024-05-16 20:08:31 -0300

    conectando api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+      .then(response => setData(response.data))
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(data)}
     </div>
   );
 }
